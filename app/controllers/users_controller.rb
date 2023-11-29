d7747489f18aaeb488dcfc04e64d2445fe3b4c2e
Mojela Ratsita <mojelaratsita1@gmail.com>
2023-11-29 17:43:42 +0200

    user profile draft

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @games = @user.participations
   end
 end
+# @games = @user.game
+# @games = @user.participations
