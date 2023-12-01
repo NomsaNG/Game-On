4e6a038dcdd96421543ca59026dc3296a4f2853b
Kieran Huybrechts <kieranhuybrechts@gmail.com>
2023-12-01 23:40:54 +0200

    modif

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   resources :venues, only: [:index, :show]
 
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: [:index, :show] do
     resources :messages, only: :create
   end
 
