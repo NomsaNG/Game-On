a1f442fb85d94def06b5616582c51ac61e0437e3
Nomsa Goba <nomsanatashagoba@gmail.com>
2023-12-05 17:57:53 +0200

    pure-sexiness

--- app/javascript/controllers/games_controller.js
+++ app/javascript/controllers/games_controller.js
@@ -1,12 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
-console.log("Stimulus is working!");
 
 export default class extends Controller {
   static targets = ["loadButton"];
   loadMore(event) {
     event.preventDefault();
-    console.log("Load More function called");
 
     document.getElementById("todays-games").style.display = "none";
     document.getElementById("tomorrows-games").style.display = "none";
@@ -14,15 +12,7 @@ export default class extends Controller {
     document.getElementById("games-list").classList.add("visible");
     document.getElementById("games-list").style.display = "block";
 
-    fetch(event.target.href, { headers: { accept: "application/json" } })
-      .then(response => response.text())
-      .then(data => {
-        console.log("Fetch URL:", event.target.href);
-
-        document.getElementById("games-list").innerHTML = data;
-
-        event.target.style.display = "none";
-      });
+    this.loadButtonTarget.style.display = "none";
+    
   }
 }
-console.log("end");
