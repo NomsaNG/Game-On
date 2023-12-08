f4ff8b94c4954e2054803922d72903c79f49659e
Nomsa Goba <nomsanatashagoba@gmail.com>
2023-12-08 14:56:15 +0200

    checking chatroom name

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,6 +2,7 @@ class Chatroom < ApplicationRecord
   belongs_to :game, optional: true
   belongs_to :community, optional: true
   has_many :messages, dependent: :destroy
+  # check if there is a name now
 
   def last_message_timestamp
     last_message&.created_at
